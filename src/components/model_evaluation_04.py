b7cd8d51a9b1258e5d9623dcef42270cac7756a2
Saurabh932 <saurabhmandhalkar03@gmail.com>
2024-02-01 01:53:33 +0530

    MlFlow added

--- src/components/model_evaluation_04.py
+++ src/components/model_evaluation_04.py
@@ -1,3 +1,4 @@
+from json import load
 import os
 import sys
 import pickle
@@ -5,12 +6,12 @@ import numpy as np
 
 from src.logger.logging import logging
 from src.exception.exception import customexception
-from src.utils import load_object
+from src.utils.utils import load_object
 
 from dataclasses import dataclass
 from pathlib import Path
 
-from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from urllib.parse import urlparse
 
 import mlflow
@@ -20,13 +21,53 @@ import mlflow.sklearn
 class ModelEvalutionConfig:
     pass
 
-class ModelEvalution():
+class ModelEvaluation():
     def __init__(self):
-        pass
+        logging.info("Evalutation has started")
 
-    def initiate_model_evalution(self):
+    def eval_metrics(self, actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        logging.info("Evalution metrics has captured")
+        return rmse, mae, r2
+
+    def initiate_model_evalution(self, train_array, test_array):
         try:
-            pass
+            X_test, y_test = (test_array[:,:-1], test_array[:,-1])
+            
+            model_path = os.path.join("artifacts", "model.pkl")
+            model = load_object(model_path)
+
+            # mlflow.set_registry_uri("")
+            logging.info("Model has register")
+
+            tracking_url_type_store = urlparse(mlflow.get_registry_uri()).scheme
+
+            print(tracking_url_type_store)
+
+            with mlflow.start_run():
+                prediction = model.predict(X_test)
+
+                (rmse, mae, r2) = self.eval_metrics(y_test, prediction)
+
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("mae", mae)
+                mlflow.log_metric("r2", r2)
+                
+                # Model registry does not work with file store
+                if tracking_url_type_store != "file":
+
+                    # Register the model
+                    # There are other ways to use the Model Registry, which depends on the use case,
+                    # please refer to the doc for more information:
+                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                    mlflow.sklearn.log_model(model, "model", registered_model_name="ml_model")
+
+                else:
+                    mlflow.sklearn.log_model(model, "model")
+
+
         except Exception as e:
             logging.info()
             raise customexception(e, sys)
--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -11,15 +11,15 @@ from src.components.model_trainer_03 import ModelTrainer
 from src.components.model_evaluation_04 import ModelEvaluation
 
 
-obj=DataIngestion()
+data_ingestion_obj=DataIngestion()
+train_data_path,test_data_path=data_ingestion_obj.initiate_data_ingestion()
 
-train_data_path,test_data_path=obj.initiate_data_ingestion()
-
-data_transformation=DataTransformation()
-
-train_arr,test_arr=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+data_transformation_obj=DataTransformation()
+train_arr,test_arr=data_transformation_obj.initiate_data_transformation(train_data_path,test_data_path)
 
 
 model_trainer_obj=ModelTrainer()
 model_trainer_obj.initate_model_training(train_arr,test_arr)
 
+model_eval_obj = ModelEvaluation()
+model_eval_obj.initiate_model_evalution(train_arr, test_arr)
